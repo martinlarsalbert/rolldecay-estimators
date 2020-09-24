ff0120bb884ed58ad95f80893ff0f6aa7a9899bf
Martin Alexandersson <marale@kth.se>
2020-09-24 11:21:23 +0200

    Implemented an Ikeda class

--- rolldecayestimators/ikeda.py
+++ rolldecayestimators/ikeda.py
@@ -1,9 +1,10 @@
 """
 This module contain a class to calculate Ikeda in various ways
 """
+import numpy as np
+import pandas as pd
 
 from rolldecayestimators import ikeda_speed
-from collections import OrderedDict
 
 class SectionsError (ValueError): pass
 
@@ -12,10 +13,149 @@ class Ikeda():
     """
     Class that helps with running various calculations known as the "Ikedas method" to predict ship roll damping.
     The idea with this class is that it can be subclassed so that varius roll damping contributions can be replaced or
-    removed.
+    removed in order to study different "flavors" of the Ikeda methods (I have seen a lot of different implementations).
 
 
     """
+
+
+    def __init__(self, V:np.ndarray, draught:float, w:np.ndarray, fi_a:float, B_W0:pd.Series, beam:float, lpp:float,
+                 kg:float, volume:float, sections:pd.DataFrame, lBK=0.0, bBK=0.0,
+                 g=9.81, rho=1000.0, visc =1.15*10**-6, **kwargs):
+        """
+        Manually specify the inputs to the calculations.
+        Note: Some inputs need to be specified here, but others can be defined in other ways
+        (by importing a ship geometry etc.)
+
+        Parameters
+        ----------
+        V
+            ship speed [m/s]
+        draught
+            ship draught [m]
+        w
+            roll frequency [rad/s]
+        fi_a
+            roll amplitude [rad]
+        B_W0 : pd.Series
+            values : wave roll damping for various frequencies [Nm*s/rad]
+            index: frequencies [rad/s]
+        beam
+            ship beam [m]
+        lpp
+            ship perpedicular length [m]
+        kg
+            vertical centre of gravity [m] (positive upward)
+        volume
+            ship displaced volume [m3]
+        sections
+            ship hull geometry sections defined in a pandas data frame:
+            sections.index  : section x coordinate measured from AP [m]
+            sections['B_s'] : sectional beam [m] (incl. both sides)
+            sections['T_s'] : sectional draught [m]
+            sections['C_s'] :   sectional area coefficient [-]
+                                C_s = S_s/(B_s*T_s)
+        lBK
+            length bilge keel [m] (=0 --> no bilge keel)
+        bBK
+            height bilge keel [m] (=0 --> no bilge keel)
+        g
+            gravity [m/s2]
+        rho
+            water density [kg/m3]
+
+
+        Returns
+        -------
+        None
+        """
+        self.V = V
+        self.draught = draught
+        self.g = g
+        self.w = w
+        self.fi_a = fi_a
+        self.B_W0=B_W0
+        self.beam=beam
+        self.lpp=lpp
+        self.kg=kg
+        self.volume = volume
+        self.sections=sections
+        self.lBK=lBK
+        self.bBK=bBK
+        self.rho=rho
+        self.visc=visc
+
+    @property
+    def OG(self):
+        """
+        distance from roll axis (cg) to still water level [m] (positive into water)
+        """
+        return self.kg - self.draught
+
+    @property
+    def Cb(self):
+        return self.volume/(self.lpp*self.beam*self.draught)
+
+    @property
+    def A_mid(self):
+        """
+        Mid section area [m2]
+        """
+        return self.A0*self.beam*self.draught
+
+    @property
+    def Ho(self):
+        """
+        half breadth to draft ratio
+        H0 = beam/(2*draught)
+        """
+        return self.beam/(2*self.draught)
+
+    @property
+    def R(self):
+        """
+        Bilge radius [m]
+        """
+        if hasattr(self,'_R'):
+            return self._R
+        else:
+            ValueError('please set the Bilge radius "R"')
+
+    @R.setter
+    def R(self, value):
+        self._R = value
+
+    @property
+    def S_s(self):
+        return self.C_s*self.T_s*self.B_s
+
+    @property
+    def A0(self):
+        """
+        mid section coefficient [-]
+        """
+        index = int(len(self.C_s)/2)
+        return self.C_s[index]
+
+    @property
+    def B_s(self):
+        # Sectional beam [m] (incl. both sides)
+        return self.sections['B_s']
+
+    @property
+    def T_s(self):
+        # Sectional draught [m]
+        return self.sections['T_s']
+
+    @property
+    def C_s(self):
+        # Sectional area coefficient: C_s = S_s/(B_s*T_s)
+        return self.sections['C_s']
+
+    @property
+    def x_s(self):
+        # Sectional x-coordinate from AP [m]
+        return self.sections.index
 
     def verify_sections(self):
 
@@ -57,17 +197,33 @@ class Ikeda():
                 )
         return B_44
 
-    def calculate_B_W(self):
+    def calculate_B_W(self, Bw_div_Bw0_max=np.inf):
         """
-        Calculate roll wave damping
+        Calculate roll wave damping at speed
 
         Returns
         -------
         B_W : ndarray
-            Roll wave damping [Nm*s/rad]
+            Roll wave damping at speed [Nm*s/rad]
 
         """
-        return 0.0
+        B_W0 = self.calculate_B_W0()
+        B_W = ikeda_speed.Bw(w=self.w, V=self.V, d=self.draught, Bw0=B_W0, g=self.g, Bw_div_Bw0_max=Bw_div_Bw0_max)
+        return B_W
+
+    def calculate_B_W0(self):
+        """
+        Calculate roll wave damping at zero speed
+
+        Returns
+        -------
+        B_W0 : ndarray
+            Roll wave damping at zero speed [Nm*s/rad]
+
+        """
+
+        w = self.B_W0.index
+        return np.interp(self.w, w, self.B_W0)  # Zero speed wave damping [Nm*s/rad]
 
     def calculate_B_F(self):
         """
@@ -79,7 +235,9 @@ class Ikeda():
             Skin friction damping [Nm*s/rad]
 
         """
-        return 0.0
+
+        return ikeda_speed.frictional(w=self.w, fi_a=self.fi_a, V=self.V, B=self.beam, d=self.draught, OG=self.OG,
+                                      ra=self.rho, Cb=self.Cb,L=self.lpp, visc=self.visc)
 
     def calculate_B_E(self):
         """
@@ -91,7 +249,13 @@ class Ikeda():
             eddy damping [Nm*s/rad]
 
         """
-        return 0.0
+        a, a_1, a_3, sigma_s, H = self.calculate_sectional_lewis_coefficients()
+
+
+        B_E = ikeda_speed.eddy(bwl=self.B_s, a_1=a_1, a_3=a_3, sigma=sigma_s, xs=self.x_s, H0=H, Ts=self.T_s, OG=self.OG,
+                               R=self.R, d=self.draught, wE=self.w, fi_a=self.fi_a)
+
+        return B_E
 
     def calculate_B_L(self):
         """
@@ -103,7 +267,8 @@ class Ikeda():
             HUl lift damping [Nm*s/rad]
 
         """
-        return 0.0
+        return ikeda_speed.hull_lift(V=self.V, d=self.draught, OG=self.OG, L=self.lpp, A=self.A_mid, ra=self.rho,
+                                     B=self.beam)
 
     def calculate_B_BK(self):
         """
@@ -115,5 +280,13 @@ class Ikeda():
             Bilge keel damping [Nm*s/rad]
 
         """
-        return 0.0
+        Bp44BK_N0, Bp44BK_H0, B44BK_L, B44BKW0 = ikeda_speed.bilge_keel(w=self.w, fi_a=self.fi_a, V=self.V, B=self.beam,
+                                                                        d=self.draught, A=self.A_mid,
+                                      bBK=self.bBK, R=self.R, g=self.g, OG=self.OG, Ho=self.Ho, ra=self.rho)
+
+        B44BK_N0 = Bp44BK_N0*self.lBK
+        B44BK_H0 = Bp44BK_H0*self.lBK
+        B44BK_L = B44BK_L
+        B44_BK = B44BK_N0 + B44BK_H0 + B44BK_L
+        return B44_BK
 
--- rolldecayestimators/tests/test_ikeda_class.py
+++ rolldecayestimators/tests/test_ikeda_class.py
@@ -1,9 +1,47 @@
+import pytest
+import pandas as pd
+import numpy as np
+from numpy.testing import assert_almost_equal
+
 from rolldecayestimators.ikeda import Ikeda
 
-def test_create_Ikeda():
-    ikeda = Ikeda()
 
-def test_calculate_Ikeda():
-    ikeda = Ikeda()
+@pytest.fixture
+def ikeda():
+
+    N=10
+    data = np.zeros(N)
+    w=np.linspace(0.1,1, N)
+    B_W0 = pd.Series(data=data, index=w)
+    fi_a = np.deg2rad(10)
+    beam=10
+    lpp=100
+    kg = 1
+    Cb=0.7
+    draught = 10
+    volume = Cb*lpp*draught*beam
+    V = 5
+    w = 0.2
+    A0 = 0.95
+
+    N_sections = 21
+    x_s = np.linspace(0, lpp, N_sections)
+    data = {
+        'B_s' : beam*np.ones(N_sections),
+        'T_s' : draught*np.ones(N_sections),
+        'C_s' : A0*np.ones(N_sections),
+    }
+    sections=pd.DataFrame(data=data, index=x_s)
+
+    i= Ikeda(V=V, draught=draught, w=w, B_W0=B_W0, fi_a=fi_a, beam=beam, lpp=lpp, kg=kg, volume=volume,
+             sections=sections)
+    i.R=2.0  # Set bilge radius manually
+
+    yield i
+
+def test_R(ikeda):
+    assert ikeda.R==2.0
+
+def test_calculate_Ikeda(ikeda):
     B_44=ikeda.calculate_B44()
 
